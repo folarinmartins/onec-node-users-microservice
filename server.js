77ef8110330cba1e606ee027ea9e5df72eb9b7f7
Folarin Martins <folarin@engineer.com>
2020-12-27 22:17:35 +0100

    Basic route augmentation for users/id

--- server.js
+++ server.js
@@ -6,26 +6,27 @@ const mongoose = require('mongoose');
 
 require('dotenv').config();
 
-const app  = express();
+const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+// app.options('*', cors()) // include before other routes
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri,{useNewUrlParser:true,useCreateIndex:true});
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
 
 const connection = mongoose.connection;
-connection.once('open',()=>{
+connection.once('open', () => {
 	console.log("MongoDB database connection established successfully");
 });
 
 const exercisesRouter = require('./routes/exercises');
 const usersRouter = require('./routes/users');
 
-app.use('/exercises',exercisesRouter);
-app.use('/users',usersRouter);
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
 
-app.listen(port, ()=>{
+app.listen(port, () => {
 	console.log(`Server is listening on port: ${port}`);
 });
